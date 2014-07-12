f09647946ef29ab0f67663586ef21ec0f60e2564
Joshua Saxton <saxtonjoshua@live.com>
2014-07-11 19:36:48 -0500

    Update adminuploadgutted.php
    Changed the header to xhtml strict 1.0

--- adminuploadgutted.php
+++ adminuploadgutted.php
@@ -1,15 +1,13 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-            "http://www.w3.org/TR/REC-html40/strict.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-<title>File Uploaded</title>
+<title>Title of document</title>
 </head>
+
 <body>
-
-
-
-
 
 <h1>File Uploaded</h1>
 
