9841d5680d726d323d35c2f727bf623d9883d4b7
Joshua Saxton <saxtonjoshua@live.com>
2014-07-11 19:48:07 -0500

    Changed XHTML strict to 1.0
    Changed to XHTML Strict 1.0 for the purposes of accessibility. Changed purpose of the SLR tool to explain purpose of SLR.

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <h1>
-            Purpose - BibtexSLR
-        </h1>
-    <p>Bibtex SLR is a program to search databases and augment bibtex entries to help
-        users perform systematic literature reviews.</p>
-    
-    
-    </body>
-</html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title>Bibtex SLR tool</title>
+</head>
+
+<body>
+    <p> Bibtex SLR is a systematic literature review tool that helps 
+    authors target literature samples with queries and visually 
+    soure the information. We hope to make the literature review 
+    process more rigorous for all, not just those in software engineering.</p>
+</body>
+
+</html>
